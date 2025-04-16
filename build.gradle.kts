7d05afd156e4a3c1215da29f23c3da1a5ce68f82
Romain Pouclet <code@romain-pouclet.com>
2025-04-16 16:47:47 -0700

    Only build snapshots

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,17 +7,8 @@ allprojects {
     repositories {
         mavenCentral()
     }
+
+    version = "1.0-SNAPSHOT"
 }
 
 group = "codes.romain"
-
-tasks.withType<PublishToMavenRepository>().configureEach {
-    doFirst {
-        val envVersion = System.getenv("VERSION")
-        if (envVersion.isNullOrBlank()) {
-            error("VERSION environment variable must be set when publishing.")
-        } else {
-            project.version = envVersion
-        }
-    }
-}
