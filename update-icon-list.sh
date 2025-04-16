d534e8056d4eafef6ae0afde0e360bf145f514bb
Romain Pouclet <code@romain-pouclet.com>
2025-04-16 14:16:21 -0700

    Ignore null

--- update-icon-list.sh
+++ update-icon-list.sh
@@ -6,4 +6,4 @@ destination="generator/src/main/resources/bootstrap-icons.json"
 github_output_file=$(mktemp)
 
 curl -L -o "$github_output_file" https://api.github.com/repos/twbs/icons/git/trees/main\?recursive\=true
-cat "$github_output_file" | jq -r '.tree | map(select(.path | startswith("icons"))) | map(.path | split("/")[1])' > "$destination"
+cat "$github_output_file" | jq -r '.tree | map(select(.path | startswith("icons/"))) | map(.path | split("/")[1])' > "$destination"
