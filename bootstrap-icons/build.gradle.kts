56fad025961f094c700fce630703df072679e6b5
Romain Pouclet <code@romain-pouclet.com>
2025-04-26 21:58:03 -0700

    Update publishing configuration

--- bootstrap-icons/build.gradle.kts
+++ bootstrap-icons/build.gradle.kts
@@ -4,6 +4,28 @@ plugins {
 }
 
 publishing {
+    repositories {
+        maven {
+            name = "releases"
+
+            url = uri("https://reposilite.dokku.perfectly-cooked.com/releases")
+            credentials(PasswordCredentials::class)
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+        }
+
+        maven {
+            name = "snapshots"
+
+            url = uri("https://reposilite.dokku.perfectly-cooked.com/snapshots")
+            credentials(PasswordCredentials::class)
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+        }
+    }
+
     publications {
         register<MavenPublication>("reposilite") {
             from(components["java"])
--- kotlinx-html-bootstrap/build.gradle.kts
+++ kotlinx-html-bootstrap/build.gradle.kts
@@ -15,4 +15,26 @@ publishing {
             from(components["java"])
         }
     }
+
+    repositories {
+        maven {
+            name = "releases"
+
+            url = uri("https://reposilite.dokku.perfectly-cooked.com/releases")
+            credentials(PasswordCredentials::class)
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+        }
+
+        maven {
+            name = "snapshots"
+
+            url = uri("https://reposilite.dokku.perfectly-cooked.com/snapshots")
+            credentials(PasswordCredentials::class)
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+        }
+    }
 }
